4881a804f925476b98268c6dcd7c4beae421ecb3
John Fedoruk <johnny@johnfedoruk.ca>
2020-03-08 16:03:04 -0500

    Setup exports

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,1 +1,8 @@
+export {
+  SinglePageRepository,
+  MultiPageRepository,
+  CmsPageFilter,
+} from '@wiser/cms-pattern';
 export { CmsModule } from './lib/cms.module';
+export { CmsService } from './lib/cms.service';
+export { DownloaderService } from './lib/downloader.service';
